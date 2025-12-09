4be4acb9999c1c1e4c6e119a679970c0eb30fbfd
Dilshan2002104-cpu <praveendilshangit@gmail.com>
2025-12-09 22:40:47 +0530

    Fix event handling and add cell filling functionality - Fixed SDL event loop to handle window close button properly - Added fill_cell function for drawing individual grid cells - Fixed compilation errors and typos - Window now responds correctly and closes without hanging

--- snake.c
+++ snake.c
@@ -11,21 +11,28 @@
 #define COLUMNS WIDTH/ CELL_SIZE
 #define LINE_WIDTH 2
 
-#define GRID_COLOR 0x1f1f1f1f
+#define COLOR_GRID 0x1f1f1f1f
+#define COLOR_WHITE 0xffffffff
 
 int draw_grid(SDL_Surface* surface)
 {
     SDL_Rect row_line = {0,0,900,LINE_WIDTH};
     for(row_line.y = 0; row_line.y < HEIGHT; row_line.y += CELL_SIZE)
     {
-        SDL_FillRect(surface,&row_line, GRID_COLOR);
+        SDL_FillRect(surface,&row_line, COLOR_GRID);
     }
 
     SDL_Rect col_line = {0,0,LINE_WIDTH,HEIGHT};
     for (col_line.x = 0; col_line.x < WIDTH; col_line.x += CELL_SIZE)
     {
-        SDL_FillRect(surface,&col_line, GRID_COLOR);
+        SDL_FillRect(surface,&col_line, COLOR_GRID);
     }
+}
+
+void fill_cell(SDL_Surface* surface, int x, int y, Uint32 color)
+{
+    SDL_Rect rect = {x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE,CELL_SIZE};
+    SDL_FillRect(surface, &rect, color);
 }
 
 int main(int argc, char* argv[])
@@ -41,14 +48,25 @@ int main(int argc, char* argv[])
     );
 
     SDL_Surface* surface = SDL_GetWindowSurface(window);
-    SDL_Rect rect = {200,200,200,200};
-    SDL_FillRect(surface, &rect, 0xffffffff);
+    
+    SDL_Event event;
+    int game = 1;
+    while (game)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+                game = 0;
+        }
 
-    draw_grid(surface);
-    SDL_UpdateWindowSurface(window);
+        fill_cell(surface,9,6,COLOR_WHITE);
 
-
-    SDL_Delay(5000);
-
-
+        draw_grid(surface);
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(20);
+    }
+    
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
